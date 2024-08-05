460200ba2c5e008d5ccfbb096a019d4c16ae3c56
leon <leon@any.com>
2024-08-05 17:51:26 -0600

    commit inicial

---
+++ src/main/java/com/prueba/ordenaris/OrdenarisApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.ordenaris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdenarisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdenarisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prueba/ordenaris/OrdenarisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.ordenaris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdenarisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
